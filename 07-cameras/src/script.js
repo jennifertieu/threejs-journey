8c63ad77175f97d5818b461bb7c68146e2489845
Jennifer Tieu <jennifer.tieuu@gmail.com>
2023-09-05 10:25:49 -0500

    add camera lesson

---
+++ 07-cameras/src/script.js
@@ -0,0 +1,99 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+
+/* NOTES
+- Camera: an abstract class, you're not supposed to use it directly
+- Array Camera: renders the scene from multiple cameras on specific areas of the render (like a multiplayer game on the same screen split)
+- Stereo Camera: renders the scene through two cameras that mimic the eyes to create a parallax effect (like VR headset, red and blue glasses, etc)
+- Cube Camera: 6 renders, each one facing a different direction, can render the surronding for things like environment map, reflection, or shadow map
+- Orthographic Camera: render the scene without perspective
+- Perspective Camera: render the scene with perspective
+ */
+
+console.log(OrbitControls);
+
+/**
+ * Base
+ */
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+// Scene
+const scene = new THREE.Scene();
+
+// Object
+const mesh = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+scene.add(mesh);
+
+// Camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  1,
+  1000
+);
+
+// const aspectRatio = sizes.width / sizes.height
+// const camera = new THREE.OrthographicCamera(- 1 * aspectRatio, 1 * aspectRatio, 1, - 1, 0.1, 100)
+
+// camera.position.x = 2
+// camera.position.y = 2
+camera.position.z = 3;
+camera.lookAt(mesh.position);
+scene.add(camera);
+
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+// Cursor
+const cursor = {
+  x: 0,
+  y: 0,
+};
+
+window.addEventListener("mousemove", (event) => {
+  cursor.x = event.clientX / sizes.width - 0.5;
+  cursor.y = -(event.clientY / sizes.height - 0.5);
+});
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+
+// Animate
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // Update camera
+  // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 2;
+  // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 2;
+  // camera.position.y = cursor.y * 3;
+  // camera.lookAt(mesh.position);
+
+  // Update objects
+  // mesh.rotation.y = elapsedTime;
+
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
